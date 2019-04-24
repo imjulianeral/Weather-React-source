a93a5ae8565b1e4ae6e8b03068e6d0ed5a8a0be5
Jorge <julianacero98@gmail.com>
2019-04-24 18:30:14 -0500

    openweather API added

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,68 @@
 import React, { Component } from 'react'
 import Header from './Header';
 import Form from './Form';
+import Error from './Error';
 
 export default class App extends Component {
+
+  state = {
+    error: false,
+    query: {},
+    resp: {}
+  }
+
+  componentDidUpdate() {
+    this.reqAPI();
+  }
+
+  reqAPI = () => {
+    const { city, country } = this.state.query;
+
+    if(!city || !country) return null;
+
+    const APIKey = 'e04e8cdb238c4678ebcb63220ecb986d';
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${APIKey}`;
+
+    fetch(url)
+      .then(resp => { return resp.json() })
+      .then(data => {
+        this.setState({
+          resp: data
+        })
+      })
+      .catch(err => console.error(err));
+  }
+  
+  dataQuery = (resp) => {
+    if (resp.city === '' || resp.country === '') {
+      this.setState({
+        error: true
+      });
+    } else {
+      this.setState({
+        query: resp
+      });
+    }
+  }
+
   render() {
+
+    let result;
+    if (this.state.error) {
+      result = <Error msg="Both fields are required"/>
+    } else {
+
+    }
+
     return (
       <div className="app">
         <Header
           title="Weather React App"
         />
-        <Form/>
+        <Form
+          dataQuery={ this.dataQuery }
+        />
+        { result }
       </div>
     )
   }
